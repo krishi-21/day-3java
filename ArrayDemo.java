481033c4eed65053c1d14bcfe24ff908decfa546
krishi-21 <22bk1a0570@stpetershyd.com>
2025-01-25 14:16:11 +0530

    first commit

---
+++ ArrayDemo.java
@@ -0,0 +1,12 @@
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int num[]= {100,200,300,100,200,300,100,200,300,100,300,200,200,100};
+		for(int i=0;i<num.length;i++) {
+			System.out.println(num[i]+"");
+		}
+
+	}
+
+}
---
+++ ArrayDemo1.java
@@ -0,0 +1,17 @@
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+		int a[]=new int[8];
+		for(int i=0,v=100;i<8;i++) {
+			a[i]=v;
+			v=v+100;
+		}
+		for(int i=0;i<8;i++) {
+			System.out.println(a[i]);
+		}
+	
+
+	}
+
+}
